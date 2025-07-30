e7b8f627f588fa394dca877d6a39322ecfd50882
Isaiah Williams <isaiah.williams5711@gmail.com>
2025-07-30 15:50:16 -0500

    Add protected and override keywords to Update function in CannonTower.cs

--- Assets/Scripts/Tower/CannonTower.cs
+++ Assets/Scripts/Tower/CannonTower.cs
@@ -4,7 +4,7 @@ public class CannonTower : Tower
 {
     [SerializeField] private GameObject cannonBallPrefab;
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         base.Update();
     }
