87cdb18745fe2b9fb9654b271dd8e134ed13ab66
Isaiah Williams <isaiah.williams5711@gmail.com>
2025-08-08 12:06:48 -0500

    Add buildButtonText variable and GetBuildButtonText() function to BuildTowerButton.cs - Add TMPro namespace

--- Assets/Scripts/Tower/BuildTowerButton.cs
+++ Assets/Scripts/Tower/BuildTowerButton.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using TMPro;
 
 public class BuildTowerButton : MonoBehaviour
 {
     [SerializeField] private GameObject heldTowerPrefab;
+    [SerializeField] private TextMeshProUGUI buildButtonText;
 
     public GameObject GetHeldTowerPrefab()
     {
         return heldTowerPrefab;
+    }
+
+    public TextMeshProUGUI GetBuildButtonText()
+    {
+        return buildButtonText;
     }
 }
