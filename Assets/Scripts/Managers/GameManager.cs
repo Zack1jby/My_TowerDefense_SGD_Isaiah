88fa960cd2ed264c64df2522ade0af3632309d8f
Isaiah Williams <isaiah.williams5711@gmail.com>
2025-08-11 21:44:29 -0500

    Add private keyword to Start() function in GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         ClearLevels();
     }
